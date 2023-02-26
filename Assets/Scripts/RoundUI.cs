a3dd9797ef7b3021a06f97bc02abe8dde89a6513
Giorgi Tskhovrebovi <g.tskhovrebovi.91@gmail.com>
2023-02-26 15:53:25 +0400

    Initial commit

---
+++ Assets/Scripts/RoundUI.cs
@@ -0,0 +1,18 @@
+﻿using BrunoMikoski.AnimationSequencer;
+using UnityEngine;
+
+public class RoundUI : MonoBehaviour
+{
+    [SerializeField] private Sequencer revealSequencer;
+    [SerializeField] private Sequencer completeSequencer;
+
+    public void Reveal()
+    {
+        revealSequencer.Play();
+    }
+    
+    public void Complete()
+    {
+        completeSequencer.Play();
+    }
+}
